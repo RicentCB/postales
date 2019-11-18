9160760741a9b4fdf2700833063ac5df826a438c
RicentCB <ricardo.cb009@gmail.com>
2019-11-18 15:37:57 -0600

    Plantilla de categorias

--- catalogo.php
+++ catalogo.php
@@ -1,136 +1,105 @@
 <?php
     include ("header.html");
 ?>
-    <main class="colorFondoMain">
-        <div class="container">
-            <div class="row colorTablaIndex">
-                <h4 class="colorLetrasAzul">Postales Virtuales: Categoria</h4>
-                <p class="grey-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit quisquam distinctio beatae quae blanditiis voluptatem eaque, ducimus laboriosam facere! Beatae deserunt quidem dolor numquam ipsa maiores assumenda nesciunt nisi eos. Lorem
-                    ipsum dolor sit amet, consectetur adipisicing elit. Officia, minima deleniti adipisci architecto reprehenderit recusandae sed ea, et doloribus quisquam nostrum consequuntur pariatur aut cum! Molestiae eos sit maxime sapiente?</p>
+    <main class="colorFondoMain" id="catalogo-Contenedor">
+        <div class="row">
+            <div class="container">
+                <h1 class="header blue-text text-darken-3">Categorias</h1>
             </div>
+            <div class="divider"></div>
+        </div>
+        <div class="container">
             <div class="row">
-                <div class="col l3">
-                    <p class="estiloCursiva tamanoLetras colorLetrasAzul">Todas las Categorias</p>
-                    <div class="colorTablaIndex">
-                        <div class="valign-wrapper">
-                            <i class="fas fa-heart prefix colorLogin"></i>
-                            <p class="grey-text estiloNegritas">SENTIMIENTOS</p>
+                <div id="seleccionar-Categoria" class="col s12 m3">
+                    <p>Todas las Categorias</p>
+                    <div class="row">
+                        <div class="contenedor-Categoria-postales col s4 m12">
+                            <p class="titulo-categoria"><i class="fas fa-heart prefix colorLogin"></i>SENTIMIENTOS</p>
+                            <ul>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                            </ul>
                         </div>
-                        <div>
-                            <a class="grey-text" href="#">Amor</a>
+                        <div class="contenedor-Categoria-postales col s4 m12">
+                            <p class="titulo-categoria"><i class="fas fa-calendar-alt prefix colorLogin"></i>Festividades</p>
+                            <ul>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                            </ul>
                         </div>
-                        <div>
-                            <a class="grey-text" href="#">Felicidad</a>
+                        <div class="contenedor-Categoria-postales col s4 m12">
+                            <p class="titulo-categoria"><i class="fas fa-heart prefix colorLogin"></i>SENTIMIENTOS</p>
+                            <ul>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                            </ul>
                         </div>
-                        <div>
-                            <a class="grey-text" href="#">Tristeza</a>
+                        <div class="contenedor-Categoria-postales col s4 m12">
+                            <p class="titulo-categoria"><i class="fas fa-heart prefix colorLogin"></i>SENTIMIENTOS</p>
+                            <ul>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                            </ul>
                         </div>
-                        <div>
-                            <a class="grey-text" href="#">Enojo</a>
+                        <div class="contenedor-Categoria-postales col s4 m12">
+                            <p class="titulo-categoria"><i class="fas fa-heart prefix colorLogin"></i>SENTIMIENTOS</p>
+                            <ul>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                                <li><a href="#">Amor</a></li>
+                            </ul>
                         </div>
-                        <hr style="border: 1px solid #fafafa;">
-                        <div class="valign-wrapper">
-                            <i class="fas fa-calendar-alt prefix colorLogin"></i>
-                            <p class="grey-text estiloNegritas">FESTIVIDADES</p>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">San Valentin</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Día de las madres</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Día de los padress</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Día del niño</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Día del abuelo</a>
-                        </div>
-                        <hr style="border: 1px solid #fafafa;">
-                        <div class="valign-wrapper">
-                            <i class="fas fa-gifts prefix colorLogin"></i>
-                            <p class="grey-text estiloNegritas">INVITACIONES</p>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Fiesta</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Boda</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Bautizos</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Posadas</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">VX años</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Fiesta infantil</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Baby shower</a>
-                        </div>
-                        <hr style="border: 1px solid #fafafa;">
-                        <div class="valign-wrapper">
-                            <i class="fas fa-birthday-cake prefix colorLogin"></i>
-                            <p class="grey-text estiloNegritas">CUMPLEAÑOS</p>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Cumpleaños niño</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Cumpleaños niña</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Cumpleaños familia</a>
-                        </div>
-                        <hr style="border: 1px solid #fafafa;">
-                        <div class="valign-wrapper">
-                            <i class="fas fa-icons prefix colorLogin"></i>
-                            <p class="grey-text estiloNegritas">VARIADAS</p>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Buenos días</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Buenas tardes</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Buenas noches</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Gracias</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Buena suerte</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Perdón</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Mejorate</a>
-                        </div>
-                        <div>
-                            <a class="grey-text" href="#">Signo Zodiacal</a>
-                        </div>
-
                     </div>
                 </div>
-                <div class="col s12 l9">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, corporis nesciunt quod amet dolores ea rerum quo? Nesciunt voluptatem repudiandae deleniti id vero dignissimos iure. Consequuntur quas doloremque deleniti tempore?</p>
+                <div id="ejemplos-Postales" class="col s12 m9 blue lighten-5">
+                    <div class="container">
+                        <!-- Titulo y Descripcion -->
+                        <div class="row">
+                            <div class="col s12">
+                                <h5 class="titulo">Sentimientos</h5>
+                                <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam a pariatur doloremque rem atque? Culpa enim animi saepe maxime dolorem fuga, minus dolor similique illum corrupti qui quidem ipsam consectetur. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Contedor de postales -->
+                    <div class="row" id="todas-postales">
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-image">
+                                  <img src="img/categorias/festividades.png">
+                                  <span class="card-title">Card Title</span>
+                                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                                </div>
+                                <div class="card-content">
+                                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-image">
+                                  <img src="img/categorias/festividades.png">
+                                  <span class="card-title">Card Title</span>
+                                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                                </div>
+                                <div class="card-content">
+                                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                                </div>
+                            </div>
+                        </div>
+                    <!-- Fin Contedor de Postales -->
+                    </div>
                 </div>
             </div>
         </div>
-
     </main>
     <?php
         include("footer.html");
     ?>
 
 </body>
-
 </html>
